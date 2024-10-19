124a89c255f6cb6fcee5686f3d50b718ede4b29b
Uveys <120497074+Uveys-Yakut@users.noreply.github.com>
2024-10-19 23:42:28 +0300

    Added footer with basic information

---
+++ resources/views/livewire/docs/footer.blade.php
@@ -0,0 +1,60 @@
+<div class="footer_wrpr">
+    <div class="ftr-content container">
+        <div class="ftr-links-row_wrpr">
+            <div class="ftr-col_cntnr">
+                <div class="ftr-title">Docs</div>
+                <ul class="ftr-itms_wrpr">
+                    <li class="ftr-itm">
+                        <a class="ftr-lnk" href="#" target="_blank" rel="noopener noreferrer">Get Started</a>
+                    </li>
+                    <li class="ftr-itm">
+                        <a class="ftr-lnk" href="#" target="_blank" rel="noopener noreferrer">Requirements</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="ftr-col_cntnr">
+                <div class="ftr-title">Community</div>
+                <ul class="ftr-itms_wrpr">
+                    <li class="ftr-itm">
+                        <a class="ftr-lnk" href="#" target="_blank" rel="noopener noreferrer">
+                            <span>
+                                Stack Overflow
+                                <svg xmlns="http://www.w3.org/2000/svg" width="13.5" height="13.5" class="ftr-externalLnkIcn" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+                                </svg>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="ftr-itm">
+                        <a class="ftr-lnk" href="#" target="_blank" rel="noopener noreferrer">
+                            <span>
+                                Twitter
+                                <svg xmlns="http://www.w3.org/2000/svg" width="13.5" height="13.5" class="ftr-externalLnkIcn" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+                                </svg>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="ftr-col_cntnr">
+                <div class="ftr-title">Scoial</div>
+                <ul class="ftr-itms_wrpr">
+                    <li class="ftr-itm">
+                        <a class="ftr-lnk" href="https://github.com/uveys-Yakut" target="_blank" rel="noopener noreferrer">
+                            <span>Github</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="13.5" height="13.5" class="ftr-externalLnkIcn" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom_wrpr">
+        <div class="ftr_copyright">
+            Copyright © 2024 Uveys-Yakut
+        </div>
+    </div>
+</div>
