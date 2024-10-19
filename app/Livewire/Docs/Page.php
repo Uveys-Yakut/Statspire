60b09db8cd2c77a1cb24f8f1ba4292dbea823add
Uveys <120497074+Uveys-Yakut@users.noreply.github.com>
2024-10-19 23:59:20 +0300

    Generate docs content dynamically using individual YAML files
    Each documentation page is now dynamically generated based on its
    corresponding YAML file. The system reads data from individual YAML files
    and updates the content based on the URL, allowing for dynamic page content.
    Updated necessary scripts and added YAML files for each page.

--- app/Livewire/Docs/Page.php
+++ app/Livewire/Docs/Page.php
@@ -4,15 +4,16 @@ namespace App\Livewire\Docs;
 
 use Livewire\Component;
 use Livewire\Attributes\On;
-use App\Helper\GlobalHelper;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
-
+use Symfony\Component\Yaml\Yaml;
 class Page extends Component
 {
     public $contentUrl;
     public $pageMetaData;
+    public $docsMenuDt;
     public $actItmContentUrl;
+    public $pageContentData;
 
     // , $description, $keywords
     public function mount($actItmContentUrl) {
@@ -22,25 +23,32 @@ class Page extends Component
     #[On('pageContent')]
     public function pageContent($urlSlug) {
         $this->actItmContentUrl = $urlSlug;
-
-        $docsMenuDt = json_decode(File::get(resource_path('data/docs/menu.json')));
+        $this->docsMenuDt = json_decode(File::get(resource_path('data/docs/menu.json')));
 
         $title = '';
         $description = '';
+        $pgCntnt_folderID = '';
+        $pgCntnt_fileID = '';
 
-        foreach ($docsMenuDt->menu as $docsMenu) {
+        foreach ($this->docsMenuDt->menu as $docsMenu) {
             foreach ($docsMenu->subItems as $subItm) {
                 if (Str::slug($subItm->title) === $urlSlug) {
                     $title = $docsMenu->title.' | '.$subItm->title;
                     $description = $docsMenu->title;
+                    $pgCntnt_folderID = $docsMenu->id;
+                    $pgCntnt_fileID = $subItm->id;
                 }
             }
         }
+
+        $pageContentYamlFile = file_get_contents(resource_path('data/docs/pages/'.$pgCntnt_folderID.'/'.$pgCntnt_fileID.'.yaml'));
+        $pageContentData = Yaml::parse($pageContentYamlFile);
 
         $this->pageMetaData = [
             'title' => $title,
             'description' => $description
         ];
+        $this->pageContentData = $pageContentData;
     }
 
     public function render()
@@ -53,7 +61,9 @@ class Page extends Component
 
         return view('livewire.docs.page', [
             'pageMetaData' => $this->pageMetaData,
-            'pageContentUrl' => $this->actItmContentUrl ?? $this->contentUrl
+            'pageContentUrl' => $this->actItmContentUrl ?? $this->contentUrl,
+            'docsMenuDt' => $this->docsMenuDt,
+            'pageContentDt' => $this->pageContentData
         ]);
     }
 }
